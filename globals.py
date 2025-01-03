2ec9ec2eff54f3b78d8827bb7b6bbfbb929a4ab0
FordenRo <FordenRo@yandex.ru>
2025-01-03 05:18:42 +0500

    Changed test bot token

--- globals.py
+++ globals.py
@@ -17,7 +17,7 @@ LOG_PATH = 'release.log' if IS_RELEASE else 'debug.log'
 FILES_PATH = 'files' if IS_RELEASE else 'testfiles'
 DATABASE_PATH = 'database.db' if IS_RELEASE else 'testdatabase.db'
 TOKEN = '7968306540:AAFxs5V5AdedRzDiMqTpn9l1etMj-16wPBo' if IS_RELEASE \
-    else '7430750632:AAGhNMrwZwTxJTiriFvOD02hxGhcMXdVLBQ'
+        else '7754920336:AAGeVhk-VSkkmRP886RLEKNVWxASoU9coeA'
 NOTIFICATION_TOKEN = '7818349534:AAFZoceGpZVyE0LO4mcAfUuRs6eikSQr_Gs'
 
 bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode='HTML',
